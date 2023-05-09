7836bd00c62794b77a4388e97d4b4b8ac9cd154b
Arjun <arjunkvarju7@.com>
2023-05-09 19:44:08 +0530

    generic calss

--- src/generics.ts
+++ src/generics.ts
@@ -28,3 +28,32 @@ let getLastUser = <User>(users:Array<User>): User => {
     let lastUser = users.pop()!;
     return lastUser;
 }
+
+//generic option with restriction
+let printUserName = <T extends User>(user:T)=> {
+    console.log(user.name)
+}
+// in the above code we can only pass the objects which have the User properties
+
+
+// generics with class
+
+interface Product {
+    name: string;
+    price: number;
+}
+
+interface Players {
+    name: string;
+    age: number;
+    game: string;
+}
+
+class BasicDetails<T> {
+    public allBasicDetails: T[] = [];
+
+    addBasicDetails(details:T) {
+        this.allBasicDetails.push(details);
+    }
+}
+//we can use the above class for Players and products or any other.
